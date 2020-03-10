e820d85f15ff411b1997783c3e321c8dd9a42054
saddamhr <saddambubt65@gmail.com>
2020-03-10 15:29:48 +0600

    tuples-sets-booleans

---
+++ 5-tuples-sets-booleans.py
@@ -0,0 +1,20 @@
+# Booleans: Booleans are just True and False
+# True -> 1
+# False -> 0
+
+# Tuples: Tuples are immutable sequences
+t = (1,2,3)
+# print(t[0])
+
+# t[0] = 'NEW' #'tuple' object does not support item assignment
+
+# Sets: Unorder collections of unique element
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(0.1)
+# x.add(4)
+# print(x)
+print(set([1,2,2,2,3,3,5]))
